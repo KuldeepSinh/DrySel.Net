80513094d0a0d9a1ba07f18db6898358c2407990
Kuldeep Sinh <kuldeep.sinh@live.com>
2020-03-15 22:29:30 -0700

    Added classes to handle Selected Items.

---
+++ DrySelCore/Actions/DeselectAll.cs
@@ -0,0 +1,17 @@
+﻿using DrySelCore.Actions.Abstractions;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace DrySelCore.Actions
+{
+    public class DeselectAll : IAction
+    {
+        public void Fire(IWebDriver webDriver, string xPath, string inputValue)
+        {
+            IWebElement webElement = webDriver.FindElement(By.XPath(xPath));
+            //create select element object
+            SelectElement selectElement = new SelectElement(webElement);
+            selectElement.DeselectAll();
+        }
+    }
+}
---
+++ DrySelCore/Assertions/ContainsSelection.cs
@@ -0,0 +1,21 @@
+﻿using DrySelCore.Assertions.Abstractions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System.Linq;
+
+namespace DrySelCore.Assertions
+{
+    public class ContainsSelection : IAssertion
+    {
+        public void Verify(IWebDriver webDriver, string xPath, string expectedValue)
+        {
+            IWebElement webElement = webDriver.FindElement(By.XPath(xPath));
+            SelectElement selectElement = new SelectElement(webElement);
+
+            Assert.IsTrue(selectElement.AllSelectedOptions
+                .Where(webElement => webElement.Text.Equals(expectedValue))
+                .Count() == 1);
+        }
+    }
+}
