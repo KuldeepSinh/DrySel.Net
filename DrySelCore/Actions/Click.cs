aa424c21b306b5144233f8a73d5d76814e53e469
Kuldeep Sinh <kuldeep.sinh@live.com>
2020-03-15 21:40:26 -0700

    Added classes to select items from drowpdown list

--- DrySelCore/Actions/Click.cs
+++ DrySelCore/Actions/Click.cs
@@ -5,7 +5,6 @@ namespace DrySelCore.Actions
 {
     public class Click : IAction
     {
-
         public void Fire(IWebDriver webDriver, string xPath, string inputValue)
         {
             IWebElement webElement = webDriver.FindElement(By.XPath(xPath));
