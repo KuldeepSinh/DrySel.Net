260a17b30cacdc3d645ebb763c3485565967abfd
Kuldeep Sinh <kuldeep.sinh@live.com>
2020-03-17 00:12:32 -0700

    Methods added to handle text Comparision.

---
+++ DrySelCore/Assertions/AreTextsNotEqual.cs
@@ -0,0 +1,15 @@
+﻿using DrySelCore.Assertions.Abstractions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+
+namespace DrySelCore.Assertions
+{
+    public class AreTextNotEqual : IAssertion
+    {
+        public void Verify(IWebDriver webDriver, string xPath, string expectedValue)
+        {
+            IWebElement webElement = webDriver.FindElement(By.XPath(xPath));
+            Assert.AreNotEqual(expectedValue, webElement.Text);
+        }
+    }
+}
