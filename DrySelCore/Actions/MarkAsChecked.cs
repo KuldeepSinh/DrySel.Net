051a9eba77e099d0b2d842ebf71dae20d40afd90
Kuldeep Sinh <kuldeep.sinh@live.com>
2020-03-17 00:06:57 -0700

    Added classes for check-box handling.

---
+++ DrySelCore/Actions/MarkAsChecked.cs
@@ -0,0 +1,27 @@
+﻿using DrySelCore.Actions.Abstractions;
+using OpenQA.Selenium;
+
+namespace DrySelCore.Actions
+{
+    public class MarkAsChecked : IAction
+    {
+        public void Fire(IWebDriver webDriver, string xPath, string inputValue)
+        {
+            IWebElement webElement = webDriver.FindElement(By.XPath(xPath));
+            ClickIfEnabled(webElement);
+        }
+
+        private void ClickIfEnabled(IWebElement webElement)
+        {
+            if (webElement.Enabled && !webElement.Selected)
+            {
+                webElement.Click();
+            }
+            else
+            {
+                throw new ElementNotInteractableException($"Not able to click {webElement.ToString()}");
+            }
+        }
+
+    }
+}
---
+++ DrySelCore/Assertions/IsSelected.cs
@@ -0,0 +1,15 @@
+﻿using DrySelCore.Assertions.Abstractions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+
+namespace DrySelCore.Assertions
+{
+    public class IsSelected : IAssertion
+    {
+        public void Verify(IWebDriver webDriver, string xPath, string expectedValue)
+        {
+            IWebElement webElement = webDriver.FindElement(By.XPath(xPath));
+            Assert.AreEqual(bool.Parse(expectedValue), webElement.Selected);
+        }
+    }
+}
